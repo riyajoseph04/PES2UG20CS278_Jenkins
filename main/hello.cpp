3f787fbcc1f157e142a0edccaffcea5bd64645f5
riyajoseph04 <87971613+riyajoseph04@users.noreply.github.com>
2023-02-14 12:28:37 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello,World\n";
   cout << "This is the second Cloud Computing assignment\n";
   cout << "I have successfully edited the code using ngrok\n";
   return 0;
